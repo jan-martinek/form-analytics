c9a009bbf32df0ff4b13068f724600af2df427e5
Jan Martinek <honza.martinek@gmail.com>
2017-01-07 10:15:33 +0100

    View decoupled

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 function SurveyData(slug, id) {
 	this.slug = slug;
 	this.id = id;
+	this.name = null;
 	
 	this.dates = [];
 	this.cases = [];
@@ -49,19 +50,114 @@ function SurveyData(slug, id) {
 	
 }
 
+function ResultsViewer(categories, survey, secondarySurvey) {
+	this.categories = categories;
+	this.survey = survey;
+	this.secondarySurvey = secondarySurvey;
+	
+	this.showResults = function()
+	{
+		var output = '<table>';
+		var qNumber = 1;
+		
+		var caseStruct = this.survey.cases[0];
+		for (var question in caseStruct) {
+			if (question == 'Časová značka') continue;
+			if (question == 'Zadejte identifikátor dotazníku') continue;			
+			
+			var values = this.survey.questions[question];
+			var desc = this.calcDescriptives(values);
+			
+			if (this.secondarySurvey && this.secondarySurvey.questions[question]) {
+				var values2 = this.secondarySurvey.questions[question];
+				var desc2 = this.calcDescriptives(values2);	
+			}
+			
+			if (this.categories[qNumber]) {
+				output += '<tr>';
+				if (this.secondarySurvey) {
+					output += '<th colspan="2" rowspan="2"><h2>' + this.categories[qNumber] + '</h2></th>';
+					output += '<th colspan="2" valign="bottom"><h2>' + this.survey.name + '</h2</th>';
+					output += '<th colspan="2" valign="bottom"><h2>' + this.secondarySurvey.name + '</h2</th>';
+					output += '</tr>';	
+					output += '<tr>';	
+					output += '<th valign="bottom">průměr</th>';
+					output += '<th valign="bottom">σ</th>';
+					output += '<th valign="bottom">průměr</th>';
+					output += '<th valign="bottom">σ</th>';
+				} else {
+					output += '<th colspan="2"><h2>' + this.categories[qNumber] + '</h2></th>';
+					output += '<th valign="bottom">průměr</th>';
+					output += '<th valign="bottom">σ</th>';
+				}
+				output += '</tr>';	
+			}
+			
+			output += '<tr>';
+			output += '<td>' + qNumber + '</td>';
+			output += '<th>' + question + '</th>';
+			output += '<td title="' + values.join(',') + '">' + desc.avg.toFixed(2) + '</td>';
+			output += '<td>' + desc.stdDev.toFixed(3) + '</td>';
+			
+			if (this.secondarySurvey && this.secondarySurvey.questions[question]) {
+				output += '<td title="' + values2.join(',') + '">' + desc2.avg.toFixed(2) + '</td>';
+				output += '<td>' + desc2.stdDev.toFixed(3) + '</td>';
+			}
+			
+			output += '</tr>';
+			qNumber++;
+		}
+		
+		output += '</table>';
+		output = this.generateDatasetDescription() + output;
+		
+		document.getElementById("content").innerHTML = output;
+		spinner.stop();	
+	}
+	
+	this.generateDatasetDescription = function() {
+		var since = new Date(this.survey.dates[0]);
+		var until = new Date(this.survey.dates[this.survey.dates.length - 1]);
+		
+		if (this.secondarySurvey) {
+			var since2 = new Date(this.secondarySurvey.dates[0]);
+			var until2 = new Date(this.secondarySurvey.dates[this.secondarySurvey.dates.length - 1]);
+			return '<p>V dotazníku <b>' + this.survey.name + '</b> bylo sebráno celkem ' + this.survey.cases.length + ' odpovědí v období od ' + since.toLocaleDateString() + ' do ' + until.toLocaleDateString() + '.<br> V dotazníku <b>' + this.secondarySurvey.name + '</b> bylo sebráno celkem ' + this.secondarySurvey.cases.length + ' odpovědí v období od ' + since2.toLocaleDateString() + ' do ' + until2.toLocaleDateString() + '.</p>';	
+		} else {
+			return '<p>Sebráno celkem ' + this.survey.cases.length + ' odpovědí v období od ' + since.toLocaleDateString() + ' do ' + until.toLocaleDateString() + '.</p>';	
+		}
+	}
+	
+	this.calcDescriptives = function(values) {
+		var sum = values.reduce(function(a, b) { return parseInt(a) + parseInt(b); });
+		var avg = sum / values.length;
+		
+		var sqDiffs = values.map(function(value){
+			var diff = parseInt(value) - avg;
+			return diff * diff;
+		});
+		var avgSqDiff = sqDiffs.reduce(function(a, b) { return a + b; }) / values.length;
+		var stdDev = Math.sqrt(avgSqDiff);
+		
+		return {
+			sum: sum,
+			avg: avg,
+			stdDev: stdDev	
+		};
+	}
+}
+
 function FormAnalytics() {
 	this.requestParams = null;
 	this.categories = {};
 	this.survey = null;
 	this.secondarySurvey = null;
-	this.surveyName = null;
-	this.secondarySurveyName = null;
 
 	this.init = function() {
 		this.requestParams = this.getQueryVariables();
 		this.prepCategories();
-		this.fetchSurveyNames();
 		this.fetchSurveyData();
+		this.fetchSurveyNames();
 	};
 	
 	this.prepCategories = function() {
@@ -97,8 +193,8 @@ function FormAnalytics() {
 	}
 	
 	this.fetchSurveyNames = function() {
-		this.surveyName = this.requestParams.name ? this.requestParams.name: 'A';
-		this.secondarySurveyName = this.requestParams.name2 ? this.requestParams.name2: 'B';
+		this.survey.name = this.requestParams.name ? this.requestParams.name: 'A';
+		this.secondarySurvey.name = this.requestParams.name2 ? this.requestParams.name2: 'B';
 	}
 	
 	this.showResultsWhenReady = function()
@@ -106,7 +202,8 @@ function FormAnalytics() {
 		var ready = this.secondarySurvey ? this.survey.dataPrepped && this.secondarySurvey.dataPrepped : this.survey.dataPrepped;
 		
 		if (ready) {
-			this.showResults();
+			var viewer = new ResultsViewer(this.categories, this.survey, this.secondarySurvey);
+			viewer.showResults();
 		} else {
 			console.log('Waiting for data.');
 			var that = this;
@@ -117,96 +214,7 @@ function FormAnalytics() {
 	}
 	
 	
-	this.showResults = function()
-	{
-		var output = '<table>';
-		var qNumber = 1;
-		
-		var caseStruct = this.survey.cases[0];
-		for (var question in caseStruct) {
-			if (question == 'Časová značka') continue;
-			if (question == 'Zadejte identifikátor dotazníku') continue;			
-			
-			var values = this.survey.questions[question];
-			var desc = this.calcDescriptives(values);
-			
-			if (this.secondarySurvey && this.secondarySurvey.questions[question]) {
-				var values2 = this.secondarySurvey.questions[question];
-				var desc2 = this.calcDescriptives(values2);	
-			}
-			
-			if (this.categories[qNumber]) {
-				output += '<tr>';
-				if (this.secondarySurvey) {
-					output += '<th colspan="2" rowspan="2"><h2>' + this.categories[qNumber] + '</h2></th>';
-					output += '<th colspan="2" valign="bottom"><h2>' + this.surveyName + '</h2</th>';
-					output += '<th colspan="2" valign="bottom"><h2>' + this.secondarySurveyName + '</h2</th>';
-					output += '</tr>';	
-					output += '<tr>';	
-					output += '<th valign="bottom">průměr</th>';
-					output += '<th valign="bottom">σ</th>';
-					output += '<th valign="bottom">průměr</th>';
-					output += '<th valign="bottom">σ</th>';
-				} else {
-					output += '<th colspan="2"><h2>' + this.categories[qNumber] + '</h2></th>';
-					output += '<th valign="bottom">průměr</th>';
-					output += '<th valign="bottom">σ</th>';
-				}
-				output += '</tr>';	
-			}
-			
-			output += '<tr>';
-			output += '<td>' + qNumber + '</td>';
-			output += '<th title="' + values.join(',') + '">' + question + '</th>';
-			output += '<td>' + desc.avg.toFixed(2) + '</td>';
-			output += '<td>' + desc.stdDev.toFixed(3) + '</td>';
-			
-			if (this.secondarySurvey && this.secondarySurvey.questions[question]) {
-				output += '<td>' + desc2.avg.toFixed(2) + '</td>';
-				output += '<td>' + desc2.stdDev.toFixed(3) + '</td>';
-			}
-			
-			output += '</tr>';
-			qNumber++;
-		}
-		
-		output += '</table>';
-		output = this.generateDatasetDescription() + output;
-		
-		document.getElementById("content").innerHTML = output;
-		spinner.stop();	
-	}
 	
-	this.generateDatasetDescription = function() {
-		var since = new Date(this.survey.dates[0]);
-		var until = new Date(this.survey.dates[this.survey.dates.length - 1]);
-		
-		if (this.secondarySurvey) {
-			var since2 = new Date(this.secondarySurvey.dates[0]);
-			var until2 = new Date(this.secondarySurvey.dates[this.secondarySurvey.dates.length - 1]);
-			return '<p>V dotazníku <b>' + this.surveyName + '</b> bylo sebráno celkem ' + this.survey.cases.length + ' odpovědí v období od ' + since.toLocaleDateString() + ' do ' + until.toLocaleDateString() + '.<br> V dotazníku <b>' + this.secondarySurveyName + '</b> bylo sebráno celkem ' + this.secondarySurvey.cases.length + ' odpovědí v období od ' + since2.toLocaleDateString() + ' do ' + until2.toLocaleDateString() + '.</p>';	
-		} else {
-			return '<p>Sebráno celkem ' + this.survey.cases.length + ' odpovědí v období od ' + since.toLocaleDateString() + ' do ' + until.toLocaleDateString() + '.</p>';	
-		}
-	}
-	
-	this.calcDescriptives = function(values) {
-		var sum = values.reduce(function(a, b) { return parseInt(a) + parseInt(b); });
-		var avg = sum / values.length;
-		
-		var sqDiffs = values.map(function(value){
-			var diff = parseInt(value) - avg;
-			return diff * diff;
-		});
-		var avgSqDiff = sqDiffs.reduce(function(a, b) { return a + b; }) / values.length;
-		var stdDev = Math.sqrt(avgSqDiff);
-		
-		return {
-			sum: sum,
-			avg: avg,
-			stdDev: stdDev	
-		};
-	}
 	
 	
 	
